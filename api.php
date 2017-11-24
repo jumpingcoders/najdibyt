e4ee047a7bdea2f385945058e15804d10ead7ad5
Jakub Rychlý <jrychly@jakubrychly.cz>
2017-11-24 23:12:47 +0100

    Update API

---
+++ api.php
@@ -0,0 +1,25 @@
+<?php
+
+if (isset($_POST['ageGroup'])) {
+	$ageGroup = $_POST['ageGroup'];
+	$localite = $_POST['localite'];
+	$size = $_POST['size'];
+	$workAdress = $_POST['workAdress'];
+	$timeWork = $_POST['timeWork'];
+	$price = $_POST['price'];
+
+	$opts = [
+    "http" => [
+        "method" => "GET",
+        "header" => "Accept-language: en\r\n" .
+            "Cookie: foo=bar\r\n" .
+            "APIKEY: f6716359e9b0d0b46afb1c47fd5b1081c9b1fc3c8e7b9762eafdda5584e5d3bb"
+	    ]
+	];
+
+	$context = stream_context_create($opts);
+
+	// Open the file using the HTTP headers set above
+	$file = file_get_contents('https://developer.o2.cz/sociodemo/api/age/10162?ageGroup=' . $ageGroup . '&occurenceType=1&hour=10', false, $context);
+
+}
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include "db/db.php";
+include "api.php";
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +11,50 @@ include "db/db.php";
 </head>
 <body>
 
+<form method="POST">
+	<div>
+		Kde checete bydlet?
+		<select name="localite">
+			<option value="metro-kacerov">Metro-Kačerov</option>
+		</select>
+	</div>
+	<div>
+	<div>
+		Velikost bytu v m2:
+		<input type="number" name="size" placeholder="Velikost bytu v m2">
+	</div>
+	<div>
+		Kde pracuješ? A jak dlouho chceš dojíždět? (v minutách)
+		<input type="text" name="workAdress" placeholder="Adresa práce"><br />
+		<input type="number" name="timeWork" placeholder="Doba dojíždění">
+	</div>
+	<div>
+		Cena bytu v korunách
+		<input type="number" name="price" placeholder="Zadejte maximální cenu bytu">
+	</div>
+	<div>
+	Věková kategorie v okolí:
+	<select name="ageGroup">
+		<option value="1">8-18</option>
+		<option value="2">19-25</option>
+		<option value="3">26-35</option>
+		<option value="4">36-55</option>
+		<option value="5">56+</option>
+	</select>
+	</div>
+	<input type="submit">
+</form>
+
+<?php
+
+if (isset($file)) {
+	echo "$file";
+}
+
+?>
+
 <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+<script src="js/api.js"></script>
 
 </body>
 </html>
