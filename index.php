64de7466f4396207c8766e521e96f8c494685359
Jakub Rychlý <jrychly@jakubrychly.cz>
2017-11-25 11:08:17 +0100

    Frontend

--- index.php
+++ index.php
@@ -96,19 +96,30 @@
       <div class="inner">
       <h2>Výsledky vyhledávání:</h2>
       <section class="features">
-      <article>
-      <h3 class="major">name</h3>
-      <p>description</p>
-      <p>location</p>
-      <p>price Kč</p>
-      <p>balkon, garaz, sklep a další tuna píčovin</p>
-      <a href="#" class="special">Link</a>
-      </article>
-      <article>
-      <h3 class="major">Nisl placerat</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
-      <a href="#" class="special">Learn more</a>
-      </article>
+      <?php
+  header('content-type: text/html; charset=utf-8');
+  $link=mysqli_connect("wm138.wedos.net", "w155086_findbyt", "WQgtnvB3", "d155086_findbyt");
+  mysqli_query($link,"SET NAMES utf8;");
+  $vysledek=mysqli_query($link,"SELECT * FROM nabidky ORDER BY uzitna_plocha;");
+  $bytu=mysqli_num_rows($vysledek);
+  for($x=0; $x<50; $x++){
+      $radek=mysqli_fetch_array($vysledek);
+      echo '<article>
+      <h3 class="major">'.$radek[2].'</h3>
+      <p>'.$radek[5].'</p>
+      <p>'.$radek[3].'</p>
+      <p>'.$radek[4].'Kč</p>
+      <p>';
+      if($radek[7]=="Ano"){echo "Balkón, ";}
+      if($radek[8]=="Ano"){echo "Bezbariérový, ";}
+      if($radek[9]=="Ano"){echo "Parkování kousek od domu, ";}
+      if($radek[10]=="Ano"){echo "Sklep, ";}
+      if($radek[11]=="Ano"){echo "Terasa.";}
+      echo '</p>
+      <a href="'.$radek[1].'" class="special">'.$radek[1].'</a>
+      </article>';
+  }
+ ?>
       </section>
       </div>
       </div>
